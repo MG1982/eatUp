d3c523478c30d5912e43bea5c6f9a3e643081b40
Mani <manigonzalez82@gmail.com>
2019-11-10 21:28:35 +0800

    small change to html and js files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -152,7 +152,7 @@ $(document).ready(function() {
     if ($("#second-option").is(":visible")) {
       startTimer(10, function() {
         console.log(
-          "TIME IS UP! - picking random dish and moving to next Model"
+          "TIME IS UP! - picking random dish and moving to next Modal"
         );
         // IF TIMER REACHES 0 RANDOM PICK FROM 4 AND MOVES TO NEXT MODAL
         let index = Math.floor(Math.random() * 3);
